293db03c813cca4fe6de3f9d4ace682e665e8536
Valeriish <valeriishtuganova@gmail.com>
2024-10-03 16:18:39 +0300

    Add function is_good_quality_threshold

--- functions/filter_fastq_module.py
+++ functions/filter_fastq_module.py
@@ -1,7 +1,13 @@
 import run_dna_rna_tools_module as tools
+
 def is_good_gc_bounds(seq:str, gc_bound:tuple=(0, 100)) -> bool:
     gc_bounds_seq = tools.gc_content(seq)
     return gc_bounds_seq in gc_bound
 
-def is_length_bounds(seq:str, length_bounds:float=0.2**32) -> bool:
+def is_good_length_bounds(seq:str, length_bounds:float=0.2**32) -> bool:
+    """Возвращает булевое значение """
     return len(seq) < length_bounds
+
+def is_good_quality_threshold(quality_seq:str, quality_threshold:float=0) -> bool:
+    quality_transform = [ord(quality) for quality in quality_seq.upper()]
+    return sum(quality_transform)/len(quality_transform) > quality_threshold
