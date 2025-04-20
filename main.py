57ae1d2484c95527ed5f5332382fa5862b3741d7
Valeriisht <138997713+Valeriisht@users.noreply.github.com>
2025-04-20 12:28:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ from NASeqTool import filter_fastq
 # Вызов данной функции приведёт к изменению свойств root logger
 logs_format = "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
 logger.add(  # Добавляем Handler для записи в stdout
-    sys.stdout, level="DEBUG", format=logs_format, colorize=True
+    sys.stdout, level="DEBUG", format=logs_format, colorize=True,
+    filter=lambda record: record["extra"].get("source") == "main" # отключение распространения
 )
 
 
