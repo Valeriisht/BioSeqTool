d5539475eae44a792b292d1e78ef5e86fe0d5989
Valeriisht <138997713+Valeriisht@users.noreply.github.com>
2025-04-20 12:24:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -108,7 +108,5 @@ def main():
         logger.critical(f"Failed with {e}")
         sys.exit(1)  # 1 - общепринятый код ошибки
 
-logger = logging.getLogger(__name__)
-
 if __name__ == "__main__":
     main()
